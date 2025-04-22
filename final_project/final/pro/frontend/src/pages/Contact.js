f3c7286bce19d8eb226b587f69928fefba32797a
Bannuru-Aman-Reddy <bannuru.charan.reddy@gmail.com>
2025-04-22 22:34:29 +0530

    contact-update

--- final_project/final/pro/frontend/src/pages/Contact.js
+++ final_project/final/pro/frontend/src/pages/Contact.js
@@ -11,25 +11,25 @@ function Contact() {
       <p className="contact-description">Phone no.:</p>
       <ul className="contact-info">
         <li>
-          <strong>Pranav Kumar Jangam:</strong> +91 75690 89985
+          <strong>Pranav Kumar Jangam:</strong> 
         </li>
         <li>
-          <strong>Voore Risheel Kumar:</strong> +91 63041 99969
+          <strong>Voore Risheel Kumar:</strong> 
         </li>
         <li>
           <strong>Akshaya:</strong> +91 70327 96574
         </li>
         <li>
-          <strong>Bheemireddy Charan Sai Reddy:</strong> +91 93815 79723
+          <strong>Bheemireddy Charan Sai Reddy:</strong>
         </li>
         <li>
-          <strong>Bannuru Charan Reddy:</strong> +91 82970 98020
+          <strong>Bannuru Charan Reddy:</strong> 
         </li>
         <li>
-          <strong>Mohammad Nida Madeeha:</strong> +91 63052 03512
+          <strong>Mohammad Nida Madeeha:</strong> 
         </li>
         <li>
-          <strong>Kolipyaka Vyshnavi:</strong> +91 63020 74473
+          <strong>Kolipyaka Vyshnavi:</strong> 
         </li>
       </ul>
     </div>
